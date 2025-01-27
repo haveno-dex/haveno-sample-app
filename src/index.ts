8df8fc6cf8faf3a33efaefca1079fecff504f521
woodser <woodser@protonmail.com>
2025-01-27 10:06:19 -0500

    add mainnet proxy config and instructions

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,13 @@ async function main() {
     console.log('Sample app using haveno-ts');
 
     // create client connected to havenod
-    let client: HavenoClient = new HavenoClient("http://localhost:8080", "apitest");
+    let client: HavenoClient = new HavenoClient("http://127.0.0.1:8080", "apitest");
 
     // get available balance
     const balances = await client.getBalances();
     console.log("Available balance: " + balances.getAvailableBalance());
+
+    // get offers
+    const offers = await client.getOffers("USD");
+    console.log("Number of USD offers: " + offers.length);
 }
